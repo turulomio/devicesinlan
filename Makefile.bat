c7520cbba6126ad9b87e80f92481098b9dfcd7c2
turulomio <turulomio@319bac0a-dde2-4268-99b8-b3fc6447b4cc>
2016-04-03 07:10:57 +0000

    Windows Makefile changed

--- Makefile.bat
+++ Makefile.bat
@@ -4,6 +4,7 @@ call c:\Python34\Lib\site-packages\PyQt5\pyuic5.bat ui/frmHelp.ui -o ui/Ui_frmHe
 call c:\Python34\Lib\site-packages\PyQt5\pyuic5.bat ui/frmMain.ui -o ui/Ui_frmMain.py
 call c:\Python34\Lib\site-packages\PyQt5\pyuic5.bat ui/frmSettings.ui -o ui/Ui_frmSettings.py
 call c:\Python34\Lib\site-packages\PyQt5\pyuic5.bat ui/frmInterfaceSelector.ui -o ui/Ui_frmInterfaceSelector.py
+call c:\Python34\Lib\site-packages\PyQt5\pyuic5.bat ui/frmDeviceCRUD.ui -o ui/Ui_frmDeviceCRUD.py
 call c:\Python34\Lib\site-packages\PyQt5\pyrcc5.exe  images/devicesinlan.qrc -o images/devicesinlan_rc.py
 call c:\Python34\Lib\site-packages\PyQt5\pylupdate5.exe -noobsolete devicesinlan.pro
 call c:\Python34\Lib\site-packages\PyQt5\lrelease.exe devicesinlan.pro
