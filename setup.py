1e95a54e6877472763487473186beac0f560af81
turulomio <turulomio@yahoo.es>
2022-07-13 20:28:30 +0200

    Added PyQt5 for linux

--- setup.py
+++ setup.py
@@ -253,7 +253,7 @@ setup(name='devicesinlan',
     install_requires= [ 'setuptools',
                         'colorama', 
                         'scapy', 
-                        'PyQt5;platform_system=="Windows"',
+                        'PyQt5',
                         'pywin32;platform_system=="Windows"',
                         ], #PyQt5 and PyQtChart doesn't have egg-info in Gentoo, so I remove it to install it with ebuild without making 2 installations. Should be added manually when using pip to install
     data_files=data_files,
