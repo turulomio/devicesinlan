9227629365d789dd51f60c28f2c548d882701fc4
turulomio <turulomio@yahoo.es>
2025-10-28 13:35:11 +0100

    devicesinlan-2.0.2 (#73)

--- devicesinlan/__init__.py
+++ devicesinlan/__init__.py
@@ -1,8 +1,8 @@
 
 from datetime import date
 
-__version__ ='2.0.1'
-__versiondate__=date(2024, 12, 28)
+__version__ ='2.0.2'
+__versiondate__=date(2025, 10, 28)
 
 
 ## This function expectss __version__= 'VERSION' file
